7f93a522bea612f8b7a242e65ef2e0bc58431570
Michael Wang <hello@mkwng.com>
2014-07-17 16:09:53 -0400

    Initial Commit

---
+++ codepathWeek6/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  codepathWeek6
+//
+//  Created by Michael Wang on 7/17/14.
+//  Copyright (c) 2014 mkwng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
